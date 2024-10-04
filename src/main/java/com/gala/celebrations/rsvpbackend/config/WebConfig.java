ade16737790cefb152f2cc3c70c2ae90e68f9976
rajmanda <raj.manda@gmail.com>
2024-10-04 17:16:39 -0400

    allowing cors to accept rajmanda-dev.com

--- src/main/java/com/gala/celebrations/rsvpbackend/config/WebConfig.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/config/WebConfig.java
@@ -10,10 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://rajmanda-dev.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
 }
 
