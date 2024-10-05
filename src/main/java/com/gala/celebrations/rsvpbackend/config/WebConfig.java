7329bef12cd912bb2ebe980a18128e1b023522f1
rajmanda <raj.manda@gmail.com>
2024-10-04 20:24:29 -0400

    removing allowing cors to accept * as we are using reverse proxy in frontend

--- src/main/java/com/gala/celebrations/rsvpbackend/config/WebConfig.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
 //                .allowedOrigins("http://localhost:4200", "https://rajmanda-dev.com")
-                .allowedOrigins("*")
+//                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
