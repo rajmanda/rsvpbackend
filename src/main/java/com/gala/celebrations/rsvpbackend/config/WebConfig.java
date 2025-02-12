a15634e23343b2991fa2ccc3041e74a6fe604519
rajmanda <raj.manda@gmail.com>
2025-02-12 19:40:15 +0000

    Adding Gala Event Backend Functionality

--- src/main/java/com/gala/celebrations/rsvpbackend/config/WebConfig.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/config/WebConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.lang.NonNull;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -18,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
 //    }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")  // Allow any domain
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
