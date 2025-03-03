cd16a3da61795bd75f860e94062089462daa9323
rajmanda <raj.manda@gmail.com>
2025-03-03 16:38:48 -0500

    Create AdminsService.java

---
+++ src/main/java/com/gala/celebrations/rsvpbackend/service/AdminsService.java
@@ -0,0 +1,39 @@
+package com.gala.celebrations.rsvpbackend.service;
+
+import com.gala.celebrations.rsvpbackend.dto.AdminsDTO;
+import com.gala.celebrations.rsvpbackend.dto.AdminsDetails;
+import com.gala.celebrations.rsvpbackend.entity.Admins;
+import com.gala.celebrations.rsvpbackend.mapper.AdminsMapper;
+import com.gala.celebrations.rsvpbackend.repo.AdminsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class AdminsService {
+
+    @Autowired
+    AdminsRepo adminsRepo;
+
+    public List<AdminsDTO> getAllAdminss() {
+        List<Admins> admins = adminsRepo.findAll();
+        return admins.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    private AdminsDTO convertToDto(Admins admins) {
+        return AdminsMapper.INSTANCE.mapAdminsToAdminsDTO(admins);
+    }
+
+    public void deleteAdmins(int adminsId) {
+        adminsRepo.deleteByAdminsId(adminsId);
+    }
+
+    public void deleteAllAdminss() {
+        adminsRepo.deleteAll();
+    }
+}
