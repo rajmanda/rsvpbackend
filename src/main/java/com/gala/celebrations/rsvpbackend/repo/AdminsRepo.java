37a93ca327ef6b0005c0acef6cce1a91c1a50c46
rajmanda <raj.manda@gmail.com>
2025-03-03 16:20:24 -0500

    Create AdminsRepo.java

---
+++ src/main/java/com/gala/celebrations/rsvpbackend/repo/AdminsRepo.java
@@ -0,0 +1,12 @@
+package com.gala.celebrations.rsvpbackend.repo;
+
+import com.gala.celebrations.rsvpbackend.entity.Admins;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminsRepo extends MongoRepository<Admins, Integer> {
+    Optional<Admins> findByAdmins_Email(String email);
+}
