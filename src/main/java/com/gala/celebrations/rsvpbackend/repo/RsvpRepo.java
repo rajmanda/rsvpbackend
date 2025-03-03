d21b2f99ff6a957292d114cbaf92ac405ecf165e
rajmanda <raj.manda@gmail.com>
2025-03-03 12:19:03 -0500

    Update RsvpRepo.java
    Adding method to check for duplicates for name / email / forGuest

--- src/main/java/com/gala/celebrations/rsvpbackend/repo/RsvpRepo.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/repo/RsvpRepo.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 @Repository
 public interface RsvpRepo extends MongoRepository<Rsvp, Integer> {
     Optional<Rsvp> findByRsvpDetails_NameAndRsvpDetails_UserEmail(String name, String userEmail);
+    Optional<Rsvp> findByRsvpDetails_NameAndRsvpDetails_UserEmailAndRsvpDetails_ForGuest(String name, String userEmail, String forGuest);
     // Delete operation by rsvpId
     void deleteByRsvpId(int rsvpId);
 }
