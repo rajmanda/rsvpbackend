a468a27fa28a78a5176549542e5daa77a2975874
rajmanda <raj.manda@gmail.com>
2025-03-03 18:11:06 -0500

    Update Admins.java

--- src/main/java/com/gala/celebrations/rsvpbackend/entity/Admins.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/entity/Admins.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "sequence")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
