2cc62eed30f988cc690853e45d88aef40f02325a
rajmanda <raj.manda@gmail.com>
2025-03-04 21:34:08 +0000

    fixing compilation issues.

--- src/main/java/com/gala/celebrations/rsvpbackend/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/config/CustomAuthenticationEntryPoint.java
@@ -4,22 +4,25 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
-        // Set the response status and content type
+    public void commence(jakarta.servlet.http.HttpServletRequest request,
+            jakarta.servlet.http.HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+    
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
 
         // Write a custom error message to the response
         System.out.println("error: Invalid or missing JWT token.");
-        response.getWriter().write("{\"error\": \"Invalid or missing JWT token.\"}");
+        throw new UnsupportedOperationException("Unimplemented method 'commence'");
     }
 }
+
