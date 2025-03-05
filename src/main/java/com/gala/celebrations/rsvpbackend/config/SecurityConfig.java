261842c9b7009f3db9bc899eec91c4eafab37b8b
Raj Manda <raj.manda@gmail.com>
2025-03-04 19:13:31 -0500

    Securing all endpoints.

--- src/main/java/com/gala/celebrations/rsvpbackend/config/SecurityConfig.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.gala.celebrations.rsvpbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,17 +20,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/public/**").permitAll()
-                .requestMatchers("/rsvp/**").permitAll()
-                .requestMatchers("/actuator/**").permitAll() 
-                .anyRequest().authenticated()
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt()
-                .and()
-                .authenticationEntryPoint(customAuthenticationEntryPoint) // Use custom entry point
-            );
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                );
         return http.build();
     }
+
 }
