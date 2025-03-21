00643b1698f5819ffd013fd05e4935d92dc6b00d
rajmanda <raj.manda@gmail.com>
2025-03-21 20:54:40 +0000

    changing cors for regex and to use origin.startsWith

--- src/main/java/com/gala/celebrations/rsvpbackend/config/CustomCorsFilter.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/config/CustomCorsFilter.java
@@ -17,15 +17,22 @@ public class CustomCorsFilter implements Filter {
             throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-
+    
         String origin = httpRequest.getHeader("Origin");
-        if (origin != null && origin.matches("https://4200-.*-vpcf\\.cloudshell\\.dev")) {
+        //if (origin != null && origin.matches("https://4200-.*-vpcf\\.cloudshell\\.dev")) {  -- Not working
+        if (origin != null && origin.startsWith("https://4200-cs-")) {
             httpResponse.setHeader("Access-Control-Allow-Origin", origin);
             httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
             httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+    
+            // Handle preflight OPTIONS request
+            if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+                httpResponse.setStatus(HttpServletResponse.SC_OK);
+                return;
+            }
         }
-
+    
         chain.doFilter(request, response);
     }
 
