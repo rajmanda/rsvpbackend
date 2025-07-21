f1478a2d1d7529d43c73a1be1de2bfb944e48673
rajmanda <raj.manda@gmail.com>
2025-07-21 10:39:46 -0400

    Update GmailConfig.java

--- src/main/java/com/gala/celebrations/rsvpbackend/config/GmailConfig.java
+++ src/main/java/com/gala/celebrations/rsvpbackend/config/GmailConfig.java
@@ -18,6 +18,8 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 @Profile("!local & !test") // Activates when profile is NOT local AND NOT test 
